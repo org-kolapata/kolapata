8406d00995675bb57b1b87721b844b74ef82118e
Umme Huzaifa <tabassum.cse777@gmail.com>
2024-06-23 13:29:36 +0600

    kolapata_product_cost_bom Updated on 23/06/2024 by Umme Huzaifa

--- kolapata_product_cost_bom/models/models.py
+++ kolapata_product_cost_bom/models/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class PosOrderLine(models.Model):
@@ -14,10 +16,19 @@ class PosOrderLine(models.Model):
             line.unit_cost_kolap = 0.0
             if line.product_id:
                 product = line.product_id
-                product.button_bom_cost()
-                # if product.bom_count > 0:
-                #     product.button_bom_cost()
+                # product.button_bom_cost()
+
+                print("product.bom_count: ", product.bom_count)
+                _logger.info("product.bom_count")
+                _logger.info(product.bom_count)
+                if product.bom_count > 0:
+                    product.button_bom_cost()
+                    _logger.info("product.bom_count2")
+                    _logger.info(product.bom_count)
                 product_cost = product.standard_price
+                print("product_cost: ", product_cost)
+                _logger.info("product_cost")
+                _logger.info(product_cost)
                 line.total_cost = line.qty * product.cost_currency_id._convert(
                     from_amount=product_cost,
                     to_currency=line.currency_id,
